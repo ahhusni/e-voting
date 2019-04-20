f88d2e4913df99ed20cccc4040804d001c89bbb9
ahhusni <h110281@gmail.com>
2019-04-20 21:46:15 +0700

    Update dbase.sql

--- dbase.sql
+++ dbase.sql
@@ -4,3 +4,11 @@ CREATE TABLE IF NOT EXISTS `candidate` (
   `directory` varchar(255) NOT NULL,
   `reg` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `voteres` (
+  `vid` int(11) NOT NULL,
+  `dirname` varchar(255) NOT NULL,
+  `selection` varchar(255) NOT NULL,
+  `lastupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `choice` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
